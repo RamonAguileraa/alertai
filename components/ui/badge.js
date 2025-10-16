007b57af09fc55656265e7ad0d6373ec04189942
RamonAguileraa <ra16monaagui16lera@gmail.com>
2025-10-16 14:27:52 -0600

    Add UI components (badge, label, table)

---
+++ components/ui/badge.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { cva } from 'class-variance-authority';
+import { cn } from '../../libs/utils';
+
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent bg-primary-500 text-white hover:bg-primary-600",
+        secondary: "border-transparent bg-gray-100 text-gray-900 hover:bg-gray-200",
+        destructive: "border-transparent bg-red-500 text-white hover:bg-red-600",
+        outline: "text-gray-950 border-gray-200",
+        success: "border-transparent bg-green-500 text-white hover:bg-green-600",
+        warning: "border-transparent bg-yellow-500 text-white hover:bg-yellow-600",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+const Badge = ({ className, variant, ...props }) => {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+};
+
+export { Badge, badgeVariants };
