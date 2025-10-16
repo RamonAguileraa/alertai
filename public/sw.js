8ab3d2361dc100d11c4f7bc6c469ccc7f212a7d4
RamonAguileraa <ra16monaagui16lera@gmail.com>
2025-10-16 14:30:11 -0600

    Add service worker for PWA

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // Placeholder: network-first strategy can be added later
+});
+
